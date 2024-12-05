fb563624f939cc58835d7e59f5f3d5ed0b038bbc
Mikhail Atuchin <matuchin@brave.com>
2024-12-05 19:56:04 +0700

    Adjust delays

--- scripts/multipage.mjs
+++ scripts/multipage.mjs
@@ -2,6 +2,7 @@
 import * as utils from './utils.mjs'
 
 export async function test(context, commands) {
+  await commands.wait.byTime(60 * 1000);
   for (const url of utils.getUrls(context)) {
     await commands.measure.start(url);
     await commands.navigate('about:blank');
