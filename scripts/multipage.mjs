5588a67696059cc87160255905358a1073cbdf99
Mikhail Atuchin <matuchin@brave.com>
2025-01-15 00:47:31 +0400

    skip broken pages in multipage

--- scripts/multipage.mjs
+++ scripts/multipage.mjs
@@ -4,7 +4,11 @@ import * as utils from './utils.mjs'
 export async function test(context, commands) {
   await commands.wait.byTime(30 * 1000);
   for (const url of utils.getUrls(context)) {
-    await commands.measure.start(url);
+    try {
+      await commands.measure.start(url);
+    } catch (e) {
+      console.error(e);
+    }
     await commands.navigate('about:blank');
     await commands.wait.byTime(5 * 1000);
   }
