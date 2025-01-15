131eb522ab09058607d6bee11778e7b679b52ca2
Mikhail Atuchin <matuchin@brave.com>
2025-01-15 16:49:33 +0400

    Try to hold more errors

--- scripts/multipage.mjs
+++ scripts/multipage.mjs
@@ -1,20 +1,28 @@
-
 import * as utils from './utils.mjs'
 
 export async function test(context, commands) {
   await commands.wait.byTime(3 * 1000);
   for (const [index, url] of utils.getUrls(context).entries()) {
     const key = new URL(url).hostname + '_' +  index;
+    let error = false
     try {
-      await commands.measure.start(url, key);
-    } catch (e) {
       await commands.measure.start(key);
+      await commands.navigate(url);
+    } catch (e) {
+      console.error(`Failed to load ${url} due to error:`, e);
+      error = true
+    }
+    await commands.measure.stop();
+
+    if (error) {
       commands.measure.addObject({
-        'error': key
+        'error': 1
       });
-      await commands.measure.stop();
-      console.error(e);
+    } else {
+      commands.measure.addObject({
+        'done': 1
+      });
     }
     await commands.navigate('about:blank');
   }
-};
+}
